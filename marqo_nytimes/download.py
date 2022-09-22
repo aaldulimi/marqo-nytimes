7d044ff51d0b86d8fc7beb1cede026baaee5f04c
Ahmed <flyinshark@Ahmeds-MacBook-Pro.local>
2022-09-22 14:41:36 +1000

    added code to scrape article content from url

--- marqo_nytimes/download.py
+++ marqo_nytimes/download.py
@@ -5,15 +5,14 @@ import xml.etree.ElementTree as ET
 
 URL = "https://www.nytimes.com/sitemap/thisweek/"
 
-headers = {
-    'user-agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.{random.randint(0, 9999)} Safari/537.{random.randint(0, 99)}'  
+header = {
+    'user-agent': "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Mobile Safari/537.36"  
 }
-
 
 def get_article_url(url):
     all_articles = []
 
-    response = requests.get(url)
+    response = requests.get(url, headers=header)
     data = response.text
 
     for node in HTMLParser(data).css('div > ul:nth-child(4) > li'):
@@ -26,8 +25,41 @@ def get_article_url(url):
     return all_articles
 
 
+def get_article_data(url):
+    response = requests.get(url, headers=header)
+    response_text = response.text
+
+    
+    if response_text:       
+        article = HTMLParser(response_text)
+
+        title = article.css_first('h1')
+        if title == None: return 
+        title = title.text()
+
+        author = article.css_first('span.last-byline')
+        if author == None: author = 'unkown' 
+        else: author = author.child.text()
+
+        body = ''
+        for node in article.css('div.StoryBodyCompanionColumn'):
+            paragraph = node.child.select('p').matches
+            for p in paragraph:
+                if p: body += p.text()
+
+
+        print("TITLE: " + title)
+        
+
+    
+    return 
+
+
+
 if __name__ == "__main__":
     article_urls = get_article_url(URL)
 
     for url in article_urls:
         print(url)
+        get_article_data(url)
+        break
